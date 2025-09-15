b1a06c4a6a55d8ec162e4b7177019709191c492e
LeTranBaoPhuc <letranbaophucxhb@gmail.com>
2025-09-15 22:30:17 +0700

    aaaa

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,9 @@ public class Main {
         for (int i = 1; i <=5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            //phucle1234566
-            //phuc213214
-
-
-            //fptunitrasddasd
+            System.out.println("i = " +565);
+            //haha
+            //pkkjdsad
         }
     }
 }
